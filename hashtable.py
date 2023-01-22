bcd6200557e184128a88050a831a8c0b74a03d3a
salomaestro <chris10an.salomonsen@gmail.com>
2023-01-22 03:49:12 +0100

    objects in python are insanely costumizable!

--- hashtable.py
+++ hashtable.py
@@ -1,4 +1,28 @@
 
+class KeyNotFound(Exception):
+    """Exception raised when a key is not found in the hashtable.
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class KeyAlreadyExists(Exception):
+    """Exception raised when a key already exists in the hashtable.
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class KeyNotModifiable(Exception):
+    """Exception raised when a key is tried to modify.
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
 class Entry:
     """
     Object storage in hash table.
@@ -29,14 +53,68 @@ class Entry:
         """
 
         if isinstance(other, str):
-            return True if self.kw == other else False
+            equal = self.kw == other
+            if equal:
+                return True
+            return False
+        
         if isinstance(other, Entry):
-            return True if hash(self) == hash(other) else False
+            equal = hash(self) == hash(other)
+            if equal:
+                return True
+            return False
+        
+        return NotImplemented
+    
+    def __iter__(self):
+        """Iterate over the values.
+        """
+
+        current = self
+
+        while current:
+            yield current
+            current = current.next_node
+
+    def __len__(self):
+        """Return the length of the values.
+        """
+
+        curr = self.next_node
+        i = 1
+        while curr:
+            i += 1
+            curr = curr.next_node
+
+        return i
+    
+    def append(self, new_entry):
+        """Append a new entry to the end of the linked list.
+
+        Args
+        ----
+            new_entry (Entry): Entry to append.
+
+        Raises
+        ------
+            KeyAlreadyExists: If the key already exists.
+        """
+
+        current = self
+
+        while current.next_node:
+            current = current.next_node
+
+            if current.kw == new_entry.kw:
+                raise KeyAlreadyExists(f"{new_entry} already exists!")
+            
+
+        current.next_node = new_entry
 
     def __str__(self):
         return f"{self.kw}"
 
-class Hashtable:
+class hashtable:
     """
     Hash table! 
     ------
@@ -55,20 +133,150 @@ class Hashtable:
         """
 
         self.max_length = max_length
-        self.entries = [None for i in range(max_length)]
+
+        self.entries = [None for _ in range(len(self))]
+        
         self.hash = kwargs.get("hash", self.__default_hash)
 
-    def __exist_entry(self, index) -> bool:
+    def __getitem__(self, index):
+        """Get item at index.
+        """
+
+        if isinstance(index, int):
+            return self.entries[index]
+        if isinstance(index, str):
+            return self.find(index)
+
+    def __setitem__(self, kw, data):
+        """Either modify or insert data.
+
+        Args
+        ----
+            kw (str): Identifier
+            data (dict): Data to store.
+        
+        Raises
+        ------
+            TypeError: If kw is not a string.
+        """
+
+        if self.modify_data(kw, **data):
+            return
+        
+        if isinstance(kw, str):
+            self.insert(kw, **data)
+
+        raise TypeError("kw must be a string!")
+
+    def __delitem__(self, kw):
+        """Delete entry of kw.
+        """
+
+        self.remove(kw)
+        
+    def __iter__(self):
+        """Iterate over the hashtable.
+        """
+
+        for entry in self.entries:
+            yield entry
+
+    def __reversed__(self):
+        """Iterate over the hashtable in reverse.
+        """
+
+        for entry in reversed(self.entries):
+            if isinstance(entry, Entry):
+                yield entry
+
+    def __contains__(self, kw) -> bool:
+        """Check if kw is in the hashtable.
+        """
+
+        try:
+            self.find(kw)
+        except KeyNotFound:
+            return False
+        else:
+            return True
+        
+    def __len__(self) -> int:
+        """Return the length of the hashtable.
+        """
+
+        return self.max_length
+
+    def __entry_exists_at_index(self, index) -> bool:
         """Check if entry exist at index.
         """
 
-        if isinstance(self.entries[index], Entry):
+        entry = self[index]
+        if isinstance(entry, Entry):
             return True
         return False
 
-    def __default_hash(self, kw:str, max_length:int) -> int:
-        # return len(kw) ** 5 % max_length
-        return hash(kw) % max_length # The modulo will map hash between 0 to max_length.
+    def __default_hash(self, kw:str) -> int:
+        
+        map_to_range = lambda x: x % len(self)
+        index = map_to_range(hash(kw))
+        return index
+    
+    def __modify_data(self, kw, **data) -> bool:
+        """Modify data of an entry.
+        
+        Args
+        ----
+            kw (str): Identifier.
+
+        Kwargs
+        -----
+            data: The data which will attach to the kw.
+
+        Returns
+        -------
+            bool: True if the data was modified, else False.
+        """
+        
+        try:
+            entry = self.find(kw)
+        except KeyNotFound as e:
+            return False
+        else:
+            entry.values.update(**data)
+        return True
+    
+    def keys(self) -> list:
+        """Return a list of all keys.
+        """
+
+        names = []
+        for entry in self:
+            if isinstance(entry, Entry):
+                for e in entry:
+                    names.append(e.kw)                
+        return names
+    
+    def values(self) -> list:
+        """Return a list of all values.
+        """
+
+        values = []
+        for entry in self:
+            if isinstance(entry, Entry):
+                for e in entry:
+                    values.append(e.values)
+        return values
+    
+    def items(self) -> list:
+        """Return a list of all items.
+        """
+
+        items = []
+        for entry in self:
+            if isinstance(entry, Entry):
+                for e in entry:
+                    items.append((e.kw, e.values))
+        return items
 
     def insert(self, kw, **data):
         """Insert a new key value pair.
@@ -76,37 +284,42 @@ class Hashtable:
         Args
         ----
             kw (str): Identifier.
+
         Kwargs
         -----
             data: The data which will attach to the kw.
+
+        Raises
+        ------
+            KeyError: If the keyword already exists.
         """
 
-        # init a new entry object
-        new = Entry(kw, **data)
+        new_entry = Entry(kw, **data)
 
         # Get the hash of the new keyword.
-        index = self.hash(kw, self.max_length)
+        index = self.hash(kw)
         
         # The simplest case:
         # There does not already exist anything
         # at the calculated index -> assign new
         # entry.
-        if not self.__exist_entry(index):
-            self.entries[index] = new
+
+        entry_exist = isinstance(self[index], Entry)
+
+        if not entry_exist:
+            
+            # Assign
+            # Must set entries[index] to new_entry, not self[index], 
+            # because self[index] will call this method again.
+            self.entries[index] = new_entry
             return
 
         # When the hash fails...
-        
-        # Get the entry that has the same hash
-        same_hash = self.entries[index]
+        # (This is a linked list)
+        # Append the new entry to the end of the linked list.
 
-        # Iterate until it does not anymore have a next_node
-        while (not same_hash.next_node is None):
-            same_hash = same_hash.next_node
+        self[index].append(new_entry)
 
-        # Assign the new entry as the next_node of the
-        # element that previously was the last element.
-        same_hash.next_node = new
 
     def find(self, kw:str) -> Entry:
         """Find and return the entry from the keyword.
@@ -119,31 +332,27 @@ class Hashtable:
         -------
             Entry: The Entry object with the data
                    of the kw.
+        
+        Raises
+        ------
+            KeyNotFound: If the keyword does not exist.
         """
 
-        # Get the hash
-        index = self.hash(kw, self.max_length)
+        index = self.hash(kw)
 
-        # Find a first contender.
-        contender = self.entries[index]
+        current_contender = self[index]
         
         # (Simple case)
-        # This will compare if the kw (str) is equal 
-        # to the contender.kw (str).
-        if contender == kw:
-            return contender
+        if current_contender == kw:
+            return current_contender
         
         # (If hash has failed)
-        # Iterate over all nodes at hash location
-        # and return if match is found.
-        while (contender.next_node):
-            contender = contender.next_node
+        for contender in self[index]:
 
             if contender == kw:
                 return contender
-        
-        # If not found.
-        raise KeyError(f"{kw} not found.")
+
+        raise KeyNotFound(f"{kw} not found.")
     
     def remove(self, kw:str) -> Entry:
         """Remove the entry with the given keyword.
@@ -156,55 +365,58 @@ class Hashtable:
         -------
             Entry: The Entry object with the data
                    of the kw.
+
+        Raises
+        ------
+            KeyNotFound: If the keyword does not exist.
         """
 
         # Get hash
-        index = self.hash(kw, self.max_length)
+        index = self.hash(kw)
 
-        contender = self.entries[index]
+        first_entry = self[index]
 
         # (Simple case)
-        # Compare if match
-        if contender == kw:
+        if first_entry == kw:
 
-            # If match then set the first Entry of the entries
-            # lsit to be the match's next node.
-            self.entries[index] = contender.next_node
-            return contender
+            # Remove
+            # Must set entries[index] to None, not self[index], 
+            # because self[index] will call insert method.
+            self.entries[index] = first_entry.next_node
+            return first_entry
 
         # (If hash has failed)
-        # Iterate as long as next node exist.
-        while (contender.next_node):
+        for contender in first_entry:
+
+            if contender.next_node != kw:
+                continue
+
+            # Store the entry to remove in temporary variable.
+            to_remove = contender.next_node
+
+            # Point the next node to remove-entry's next node.
+            contender.next_node = to_remove.next_node
+
+            # pop
+            return to_remove
             
-            # If match
-            if contender.next_node == kw:
-
-                # Store the entry to remove in temporary variable.
-                to_remove = contender.next_node
-
-                # Point the next node to remove-entry's next node.
-                contender.next_node = to_remove.next_node
-
-                # pop
-                return to_remove
-            
-            # Update
-            contender = contender.next_node
-        
         # Not found.
         raise KeyError(f"{kw} not found.")
     
     def __get_longest_kw(self) -> int:
+        """Get the longest keyword in the hashtable.
+        """
+
         longest = 0
-        for e in self.entries:
+        for e in self:
             if (not e):
                 continue
 
             longest = max(0, len(e.kw))
 
-            while (e.next_node):
-                longest = max(0, len(e.kw))
-                e = e.next_node
+            for linked_e in e:
+                longest = max(0, len(linked_e.kw))
+
         return longest
 
     def __str__(self) -> str:
@@ -212,7 +424,8 @@ class Hashtable:
 
         out = "Hashtable(\n"
 
-        for i, e in zip(range(self.max_length), self.entries):
+        for i, e in enumerate(self):
+
             e_fmt = f"{e}" if e else ""
             out += f"\t{i} --- {e_fmt:^{l+2}}"
 
@@ -220,11 +433,14 @@ class Hashtable:
                 out += "\n"
                 continue
 
-            while (e.next_node):
-                e_fmt = f"{e.next_node}"
+            for j, linked_e in enumerate(e):
+                if j == 0:
+                    continue
+
+                e_fmt = f"{linked_e}"
                 out += f" --- {e_fmt:^{l+2}}"
-                e = e.next_node
 
             out += "\n"
         out += ")"
+
         return out
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
-from hashtable import Hashtable, Entry
+from hashtable import hashtable
 
-names = ["Jonny", "Reka", "Richardt", "Rånny", "Turid", "Laila", \
+names = ["Raymond", "Reka", "Richardt", "Rånny", "Turid", "Laila", \
          "Kenneth", "Pølsa", "Hjørdis", "Peder", "Finn-Åge", "Gjerdrun", \
          "Finnlugg", "Jesus Kristus"]
 sexes = ["M", "F", "M", "M", "F", "F", "M", "M", "F", "M", "M", "F", "M", "F"]
 ages = [random.randint(27, 63) for _ in range(len(names)-1)]; ages.append(2023)
 people = [(name, age, sex) for name, age, sex in zip(names, ages, sexes)]
 
-table = Hashtable(5)
+table = hashtable(len(names) - 5)
 
 for name, age, sex in people:
     data = {"age": age, "sex": sex}
@@ -16,8 +16,17 @@ for name, age, sex in people:
 
 print(table)
 
-print(table.find("Reka"))
+print("Reka" in table) # Or use table.find("Reka") to get the data.
 print(table.find("Jesus Kristus").values)
 
-print(table.remove("Finn-Åge"))
+del table["Finn-Åge"] # or popped = table.remove("Finn-Åge")
+
 print(table)
+
+print(table["Turid"])
+
+# print(table.keys())
+
+# print(table.values())
+
+# print(table.items())
